2fa2ec09984c290edb91fea21a4fb6bf23344d4b
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-03-26 18:56:00 +0500

    Completed

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume-slider input"),
+keysCheckbox = document.querySelector(".keys-checkbox input");
 
 
-let audio = new Audio("tunes/a.wav");           //BY DEFAULT AUDIO SRC IS A TONE
+let allKeys = [],
+audio = new Audio("tunes/a.wav");           //BY DEFAULT AUDIO SRC IS A TONE
 
 
 
@@ -10,7 +13,12 @@ const playTune = (key) => {
     audio.src = `tunes/${key}.wav`;         // PADDING AUDIO SRC BASED ON PASSED KEY
     audio.play();
 
-    const clicked = document.querySelector(`[data-key="${key}"]`);
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 }
 
 
@@ -20,15 +28,29 @@ const playTune = (key) => {
 
 pianoKeys.forEach(key => {
 
+    allKeys.push(key.dataset.key);
+
     // CALLING PLAYTUNE FUNCTION BY PASSING THE DATASET VALUE AS ARGUMENT
     key.addEventListener("click", () => playTune(key.dataset.key))
 
 });
 
 
-const pressedKey = (e) => {
-    playTune(e.key);
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
 }
 
 
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+
+const pressedKey = (e) => {
+    if(allKeys.includes(e.key)) playTune(e.key);
+}
+
+
+keysCheckbox.addEventListener("click", showHideKeys);
+volumeSlider.addEventListener("input", handleVolume);
 document.addEventListener("keydown", pressedKey);
